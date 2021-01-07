ddea38c381df7316e220715248eff7b8857d7171
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-07 00:28:49 -0600

    doesn't quite work yet but good night

---
+++ README.md
@@ -0,0 +1,1 @@
+Going on Facebook is often a boring and useless experience. So I decided to make this Chrome extension, even though I don't use Chrome. Now you can make chaotic art as you go on Facebook!
---
+++ index.js
@@ -0,0 +1,55 @@
+let plot = document.getElementById('plot')
+let x = []
+let y = []
+
+function draw_plot(z) {
+    for (let i = 0; i < window.innerWidth + 70; i++) {
+        for (let j = 0; j < window.innerHeight; j++) {
+            x.push(i)
+            y.push(j)
+        }
+    }
+    for (let i = 0; i < y.length; i++) {
+        y[i] *= -1
+    }
+    let data = [{
+        x: x,
+        y: y,
+        z: z,
+        type: 'contour',
+        contours: {
+            coloring: 'lines'
+        },
+        colorscale: [
+            ['0', '#ffffff'],
+            ['1', '#ffffff']
+        ],
+        colorbar: {
+            thickness: 0
+        }
+    }];
+
+    let layout = {
+        width: window.innerWidth + 70,
+        height: window.innerHeight,
+        xaxis: {
+            visible: false
+        },
+        yaxis: {
+            visible: false
+        },
+        coloraxis_showScale: false,
+        margin: {
+            t: 0,
+            r: 0,
+            b: 0,
+            l: 0
+        },
+        paper_bgcolor: '#000000',
+        plot_bgcolor: '#000000'
+    };
+
+    Plotly.newPlot('plot', data, layout, {
+        displayModeBar: false
+    });
+}
