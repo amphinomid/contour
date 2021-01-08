51618bd5b9671bcd6bdad3c93249557702d9185f
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-08 11:11:16 -0600

    ?

--- background.js
+++ background.js
@@ -1,86 +1,27 @@
-let width, height
 let z
 let scroll_dist = 0
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.windows.getCurrent(function (win) {
-        width = win.width
-        height = win.height
         chrome.storage.local.set({ toggle: '●' })
-        init()
+        init(win.width, win.height)
 
         // click
-        chrome.windows.addEventListener('click', function (e) {
-            console.log('onclick')
-            chrome.storage.local.get('toggle', function (result) {
-                if (result.toggle == '■' && e.target.id != 'toggle') {
-                    let mouse_x = e.clientX
-                    let mouse_y = e.clientY
-                    // console.log(mouse_x + ', ' + mouse_y)
-                    let radius = 75
-                    for (let dx = -radius; dx <= radius; dx++) {
-                        for (let dy = -radius; dy <= radius; dy++) {
-                            let new_x = mouse_x + dx
-                            let new_y = mouse_y + dy
-                            if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
-                                let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
-                                if (dist <= radius) {
-                                    z[new_x * height + new_y] += radius - dist
-                                    // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
-                                }
-                            }
-                        }
-                    }
-                }
-            })
+        win.addEventListener('click', function (e) {
+            console.log('click')
+            click(e, win.width, win.height)
         })
 
         // move
-        chrome.windows.addEventListener('mousemove', function (e) {
-            console.log('onmousemove')
-            chrome.storage.local.get('toggle', function (result) {
-                if (result.toggle == '■') {
-                    let mouse_x = e.clientX
-                    let mouse_y = e.clientY
-                    // console.log(mouse_x + ', ' + mouse_y)
-                    let radius = 50
-                    for (let dx = -radius; dx <= radius; dx++) {
-                        for (let dy = -radius; dy <= radius; dy++) {
-                            let new_x = mouse_x + dx
-                            let new_y = mouse_y + dy
-                            if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
-                                let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
-                                if (dist <= radius) {
-                                    z[new_x * height + new_y] += radius - dist
-                                    // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
-                                }
-                            }
-                        }
-                    }
-                }
-            })
+        win.addEventListener('mousemove', function (e) {
+            console.log('mousemove')
+            move(e, win.width, win.height)
         })
 
         // scroll
-        chrome.windows.addEventListener('scroll', function () {
-            console.log('onscroll')
-            chrome.storage.local.get('toggle', function (result) {
-                if (result.toggle == '■') {
-                    scroll_dist += window.scrollY // later: modify to include change (for scrolling up)
-                    // console.log(scroll_dist)
-                    let added = 0
-                    let interval = (width + 70 - scroll_dist) / 5
-                    for (let i = 0; i < width + 70; i++) {
-                        if (i % Math.max(interval, 1) == 0) {
-                            added += 10
-                            // console.log(i + ': ' + added)
-                        }
-                        for (let j = 0; j < height; j++) {
-                            z[i * height + j] += added
-                        }
-                    }
-                }
-            })
+        win.addEventListener('scroll', function () {
+            console.log('scroll')
+            scroll(win.width, win.height)
         })
     })
 })
@@ -99,7 +40,7 @@ chrome.storage.onChanged.addListener(function (changes) {
 })
 
 // initialize flat plane
-function init() {
+function init(width, height) {
     z = []
     for (let i = 0; i < width + 70; i++) {
         for (let j = 0; j < height; j++) {
@@ -109,75 +50,72 @@ function init() {
 }
 
 // click
-// window.onclick = function (e) {
-//     console.log('onclick')
-//     chrome.storage.local.get('toggle', function (result) {
-//         if (result.toggle == '■' && e.target.id != 'toggle') {
-//             let mouse_x = e.clientX
-//             let mouse_y = e.clientY
-//             // console.log(mouse_x + ', ' + mouse_y)
-//             let radius = 75
-//             for (let dx = -radius; dx <= radius; dx++) {
-//                 for (let dy = -radius; dy <= radius; dy++) {
-//                     let new_x = mouse_x + dx
-//                     let new_y = mouse_y + dy
-//                     if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
-//                         let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
-//                         if (dist <= radius) {
-//                             z[new_x * height + new_y] += radius - dist
-//                             // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     })
-// }
+function click(e, width, height) {
+    chrome.storage.local.get('toggle', function (result) {
+        if (result.toggle == '■' && e.target.id != 'toggle') {
+            let mouse_x = e.clientX
+            let mouse_y = e.clientY
+            // console.log(mouse_x + ', ' + mouse_y)
+            let radius = 75
+            for (let dx = -radius; dx <= radius; dx++) {
+                for (let dy = -radius; dy <= radius; dy++) {
+                    let new_x = mouse_x + dx
+                    let new_y = mouse_y + dy
+                    if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
+                        let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
+                        if (dist <= radius) {
+                            z[new_x * height + new_y] += radius - dist
+                            // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
 
 // move
-// window.onmousemove = function (e) {
-//     console.log('onmousemove')
-//     chrome.storage.local.get('toggle', function (result) {
-//         if (result.toggle == '■') {
-//             let mouse_x = e.clientX
-//             let mouse_y = e.clientY
-//             // console.log(mouse_x + ', ' + mouse_y)
-//             let radius = 50
-//             for (let dx = -radius; dx <= radius; dx++) {
-//                 for (let dy = -radius; dy <= radius; dy++) {
-//                     let new_x = mouse_x + dx
-//                     let new_y = mouse_y + dy
-//                     if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
-//                         let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
-//                         if (dist <= radius) {
-//                             z[new_x * height + new_y] += radius - dist
-//                             // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     })
-// }
+function move(e, width, height) {
+    chrome.storage.local.get('toggle', function (result) {
+        if (result.toggle == '■') {
+            let mouse_x = e.clientX
+            let mouse_y = e.clientY
+            // console.log(mouse_x + ', ' + mouse_y)
+            let radius = 50
+            for (let dx = -radius; dx <= radius; dx++) {
+                for (let dy = -radius; dy <= radius; dy++) {
+                    let new_x = mouse_x + dx
+                    let new_y = mouse_y + dy
+                    if (new_x >= 0 && new_x < width + 70 && new_y >= 0 && new_y < height) {
+                        let dist = Math.sqrt((new_x - mouse_x) * (new_x - mouse_x) + (new_y - mouse_y) * (new_y - mouse_y))
+                        if (dist <= radius) {
+                            z[new_x * height + new_y] += radius - dist
+                            // console.log(new_x + ', ' + new_y + ', ' + z[new_x * height + new_y])
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
 
 // scroll
-// window.onscroll = function () {
-//     console.log('onscroll')
-//     chrome.storage.local.get('toggle', function (result) {
-//         if (result.toggle == '■') {
-//             scroll_dist += window.scrollY // later: modify to include change (for scrolling up)
-//             // console.log(scroll_dist)
-//             let added = 0
-//             let interval = (width + 70 - scroll_dist) / 5
-//             for (let i = 0; i < width + 70; i++) {
-//                 if (i % Math.max(interval, 1) == 0) {
-//                     added += 10
-//                     // console.log(i + ': ' + added)
-//                 }
-//                 for (let j = 0; j < height; j++) {
-//                     z[i * height + j] += added
-//                 }
-//             }
-//         }
-//     })
-// }
+function scroll(width, height) {
+    chrome.storage.local.get('toggle', function (result) {
+        if (result.toggle == '■') {
+            scroll_dist += window.scrollY // later: modify to include change (for scrolling up)
+            // console.log(scroll_dist)
+            let added = 0
+            let interval = (width + 70 - scroll_dist) / 5
+            for (let i = 0; i < width + 70; i++) {
+                if (i % Math.max(interval, 1) == 0) {
+                    added += 10
+                    // console.log(i + ': ' + added)
+                }
+                for (let j = 0; j < height; j++) {
+                    z[i * height + j] += added
+                }
+            }
+        }
+    })
+}
